61fc91c62adb9043af0f11a2b396c5b67c925e97
sph3 <59633047+sph3@users.noreply.github.com>
2022-05-10 22:44:51 -0300

    Add nodemon and npm script

--- fundamentos/src/index.js
+++ fundamentos/src/index.js
@@ -3,9 +3,9 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  return res.json({ message: 'Hello, World!' });
+  return res.json({ message: 'Hello, World!', test: 'tested!' });
 });
 
-const PORT = 3333;
 // localhost:3333
+const PORT = 3333;
 app.listen(PORT);

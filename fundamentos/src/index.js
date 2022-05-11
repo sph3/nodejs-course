ec9a28e4764f955ea211d92b58c6b636e43eddb4
sph3 <59633047+sph3@users.noreply.github.com>
2022-05-10 23:10:52 -0300

    Add other http methods

--- fundamentos/src/index.js
+++ fundamentos/src/index.js
@@ -1,9 +1,26 @@
 const express = require('express');
+const res = require('express/lib/response');
 
 const app = express();
 
-app.get('/', (req, res) => {
-  return res.json({ message: 'Hello, World!', test: 'tested!' });
+app.get('/courses', (req, res) => {
+  return res.json(['Curso 1', 'Curso 2', 'Curso 3']);
+});
+
+app.post('/courses', (req, res) => {
+  return res.json(['Curso 1', 'Curso 2', 'Curso 3', 'Curso 4']);
+});
+
+app.put('/courses/:id', (req, res) => {
+  return res.json(['Curso 6', 'Curso 2', 'Curso 3', 'Curso 4']);
+});
+
+app.patch('/courses/:id', (req, res) => {
+  return res.json(['Curso 6', 'Curso 7', 'Curso 3', 'Curso 4']);
+});
+
+app.delete('/courses/:id', (req, res) => {
+  return res.json(['Curso 6', 'Curso 7', 'Curso 4']);
 });
 
 // localhost:3333

025eccc952fb9bfcc0088b3f1e6019e18fae68f9
sph3 <59633047+sph3@users.noreply.github.com>
2022-05-12 20:36:40 -0300

    Fundamentos finished

--- fundamentos/src/index.js
+++ fundamentos/src/index.js
@@ -2,20 +2,41 @@ const express = require('express');
 const res = require('express/lib/response');
 
 const app = express();
+app.use(express.json());
+
+/**
+ * Route params
+ * 'route/:param'
+ * req.params
+ *
+ * Query params
+ * 'route?param=value&another_param=2'
+ * req.query
+ *
+ * Body params
+ * json: { "key": "value" }
+ * req.body
+ */
 
 app.get('/courses', (req, res) => {
   return res.json(['Curso 1', 'Curso 2', 'Curso 3']);
 });
 
 app.post('/courses', (req, res) => {
+  const body = req.body;
+  console.log(body);
   return res.json(['Curso 1', 'Curso 2', 'Curso 3', 'Curso 4']);
 });
 
 app.put('/courses/:id', (req, res) => {
+  const { id } = req.params;
+  console.log('id:', id);
   return res.json(['Curso 6', 'Curso 2', 'Curso 3', 'Curso 4']);
 });
 
 app.patch('/courses/:id', (req, res) => {
+  const { page } = req.query;
+  console.log('page:', page);
   return res.json(['Curso 6', 'Curso 7', 'Curso 3', 'Curso 4']);
 });
 
